5232b62ab5126397c246acec488bf85b5dab49cc
Muhammad Ali <malishoaib.2001@gmail.com>
2025-07-28 02:06:49 +0500

    Started

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../../components/navbar";
+
+const Landing = () => {
+  return (
+    <div className="w-full min-h-screen bg-red-500"> 
+      <Navbar />;
+    </div>
+  );
+};
+
+export default Landing;
